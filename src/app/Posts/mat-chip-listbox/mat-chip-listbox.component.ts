3974634eed5c9655ed2ca9558a2a1a9b399a4ff1
Luis Antonio <varelasantonio5308@gmail.com>
2023-09-20 14:28:04 -0600

    Co-authored-by: josuemonjaras <josuemonjaras@users.noreply.github.com>

--- src/app/Posts/mat-chip-listbox/mat-chip-listbox.component.ts
+++ src/app/Posts/mat-chip-listbox/mat-chip-listbox.component.ts
@@ -1,10 +1,10 @@
 import {Component} from '@angular/core';
 import {MatChipsModule} from '@angular/material/chips';
 
-
 @Component({
   selector: 'app-mat-chip-listbox',
   templateUrl: 'mat-chip-listbox.component.html',
   styleUrls: ['./mat-chip-listbox.component.css']
 })
-export class ChipsOverviewExample {}
+
+export class MatChipListboxComponent{}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
 
 import { Subject } from 'rxjs';
 
 import { FooterComponent } from './Posts/footer/footer.component';
+import { DialogComponent } from './Posts/dialog/dialog.component';
+
+
+import { MatChipListboxComponent } from './Posts/mat-chip-listbox/mat-chip-listbox.component';
+import { ToolbarOverviewExample } from './Posts/toolbar-overview-example/toolbar.component';
+import { TableFilteringExample } from './Posts/table-filtering-example/table-filtering-example';
+
+
+
 
 
 
@@ -15,13 +27,22 @@ import { FooterComponent } from './Posts/footer/footer.component';
   declarations: [
     AppComponent,
     FooterComponent,
+    MatChipListboxComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ToolbarOverviewExample,
+    MatChipsModule,
+    MatDialogModule,
+    TableFilteringExample,
+    MatButtonModule,
+    DialogComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { }import { MatChipListbox } from '@angular/material/chips';
+
+
