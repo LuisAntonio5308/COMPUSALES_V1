6c53fb4dae57997701d0302002ca510825dced03
LuisAntonio5308 <varelasantonio5308@gmail.com>
2023-09-22 19:34:28 -0600

    Publicaciones

---
+++ src/app/Posts/Element-Post/post-list/post-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnDestroy, OnInit, Output } from "@angular/core";
+import { Post } from "../../post.model";
+import { PostService } from "../../posts.service";
+import {Subscription} from "rxjs";
+
+@Component({
+    selector: 'app-post-list',
+    templateUrl: './post-list.component.html',
+    styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent implements OnInit, OnDestroy{
+    posts: Post[] = [];
+    private postsSub: Subscription;
+
+    constructor(public postsService: PostService){}
+    ngOnInit(){
+        this.posts = this.postsService.getPosts();
+        this.postsSub = this.postsService.getPostUpdateListerner()
+        .subscribe((posts: Post[]) =>{
+            this.posts = posts
+        });
+    }
+
+    ngOnDestroy(){
+        this.postsSub.unsubscribe();
+    }
+
+    //Eliminar Publicacion
+    eliminarPublicacion(post: Post) {
+        this.postsService.deletePost(post);
+      }
+      
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,6 +15,9 @@ import {MatButtonModule} from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { PostService } from './Posts/posts.service';
+import { PostListComponent } from './Posts/Element-Post/post-list/post-list.component';
+import { PostCreateComponent } from './Posts/Element-Post/post-create/post-create.component';
 
 
 
@@ -19,7 +27,7 @@ import { Subject } from 'rxjs';
 import { FooterComponent } from './Posts/footer/footer.component';
 import { DialogComponent } from './Posts/dialog/dialog.component';
 import { TreeComponent } from './Posts/tree/tree.component';
-
+import { MatChipListbox } from '@angular/material/chips';
 import { MatChipListboxComponent } from './Posts/mat-chip-listbox/mat-chip-listbox.component';
 import { TableFilteringExample } from './Posts/table-filtering-example/table-filtering-example';
 import { ProgressBarConfigurableExample } from './Posts/progress-bar-indeterminate-example/progress-bar';
@@ -28,12 +36,17 @@ import { ToolbarOverviewExample } from './Posts/toolbar-overview-example/toolbar
 
 
 
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     MatChipListboxComponent,
-    ToolbarOverviewExample
+    ToolbarOverviewExample,
+    PostListComponent,
+    PostCreateComponent
     
   ],
   imports: [
@@ -50,13 +63,15 @@ import { ToolbarOverviewExample } from './Posts/toolbar-overview-example/toolbar
     ProgressBarConfigurableExample,
     MatToolbarModule,
     MatIconModule,
-    MatMenuModule
+    MatMenuModule,
+    FormsModule
 
   ],
-  providers: [],
+  providers: [PostService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }import { MatChipListbox } from '@angular/material/chips';
+export class AppModule { }
+
 
 
 
